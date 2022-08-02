49846b1f0919faa4b7b5c966f66d4be160cb2b28
StanojevicAleksandar <109697885+StanojevicAleksandar@users.noreply.github.com>
2022-08-02 05:21:10 +0200

    Fixing JS

--- app.js
+++ app.js
@@ -1,43 +1,47 @@
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = 'api_key=a199b4f09b3e7d29e56d2330f8014163';
 const API_URL = BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
-const IMG_URL = "heetps://image.tmdb.org/t/p/w500";
+const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
-const row_recommendations = document.getElementById("recommendations");
+const main = document.getElementById("main");
 
 getMovies(API_URL);
 
 function getMovies(url) {
   fetch(url).then(res => res.json()).then(data => {
+    console.log(data.results)
     showMovies(data.results);
   })
 }
 
 function showMovies(data) {
-  row_recommendations.innerHTML = "";
+  main.innerHTML = "";
 
   data.forEach(movie => {
     const { title, poster_path, vote_average, overview } = movie;
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
     movieEl.innerHTML = `
-  
-  <img
-  src="${IMG_URL + poster_path}" class="recommendations__img" alt="${title}"/>
+    
+    <div class="movie__container">
+    <div class="movie">
+      <img src="${IMG_URL + poster_path}" alt="${title}"
+      />
 
-  <div class="movie__info">
-  <h3>${title}</h3>
-  <span class="${getColor(vote_average)}">${vote_average}</span>
-  </div>
-  
-  <div class="overview">
-  
-  <h3>Overview</h3>
-  ${overview}
-  </div>
+      <div class="movie__info">
+        <h3>${title}</h3>
+        <span class="${getColor(vote_average)}">${vote_average}</span>
+      </div>
+
+      <div class="overview">
+
+        <h3>Overview</h3>
+        ${overview}
+      </div>
+
   `
 
-  row_recommendations.appendChild(movieEl);
+  main.appendChild(movieEl);
 
   })
 }
@@ -50,4 +54,4 @@ function getColor(vote) {
   } else {
     return "red"
   }
-}
+} 
